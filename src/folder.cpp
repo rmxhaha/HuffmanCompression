ba52c6099998e61339ff52454ea1fcab9560ab3b
rmxhaha <code000123@gmail.com>
2016-02-08 09:39:30 +0700

    restructure project folder; add api call for listing files;

---
+++ src/folder.cpp
@@ -0,0 +1,32 @@
+#include "include/folder.h"
+
+int readFile(){
+  DIR           *d;
+  struct dirent *dir;
+  d = opendir(".");
+  if (d)
+  {
+    while ((dir = readdir(d)) != NULL)
+    {
+      printf("%s\n", dir->d_name);
+    }
+
+    closedir(d);
+  }
+
+  return(0);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
