fe4694f6f427d0778b8fecea704f968ff207c3ba
rmxhaha <code000123@gmail.com>
2016-02-18 20:51:59 +0700

    code to test bitwriter and bitreader

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,8 @@
 #include "../include/file.hpp"
 #include "../include/header.hpp"
 #include "../include/archieve.hpp"
-
+#include "../include/bitreader.hpp"
+#include "../include/bitwriter.hpp"
 #include <iostream>
 #include <fstream>
 using namespace std;
@@ -11,6 +12,21 @@ using namespace std;
 int main(){
     Header head;
     head = buildHead("sample");
+    ifstream infile("bitread.sample", ios::in | ios::binary);
+    ofstream outfile("bitwrite.sample", ios::binary | ios::out );
+    BitReader reader(infile);
+    BitWriter writer(outfile);
+    int x = 0;
+    while( !reader.eof() ){
+        if( x ++ % 8 == 0) cout << endl;
+        bool b = reader.readBit();
+        cout << b;
+        writer.writeBit(b);
+    }
+
+    outfile.close();
+
+
 /*
     for( auto& it : head.folders ){
         cout << it.folderName << endl;
@@ -23,6 +39,7 @@ int main(){
     cout << endl;
     printHead(head);
 */
+/*
     printHead(head);
     ofstream outfile;
     outfile.open ("sample.hu", ios::out | ios::binary);
@@ -30,15 +47,16 @@ int main(){
     fillArchieve(head,outfile,"sample");
     outfile.close();
 
-    ifstream infile("sample.hu", ios::in | ios::binary);
     head.files.clear();
     head.folders.clear();
+
+    ifstream infile("sample.hu", ios::in | ios::binary);
     Bin2Header(head,infile);
     buildFolder(head,"sample2");
     extractArchieve(head,infile,"sample2");
     infile.close();
 
     printHead(head);
-
+*/
 	return 0;
 }
