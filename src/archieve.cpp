a7ed6463b076c64e631a51b5138e9a0e9cb465d9
rmxhaha <code000123@gmail.com>
2016-02-21 09:49:45 +0700

    adding ratio overall ratio info

--- src/archieve.cpp
+++ src/archieve.cpp
@@ -10,6 +10,10 @@ using namespace std;
 
 void fillArchieve( Header& header, BitWriter& writer, const string& ftarget ){
     int fileidx = 0;
+    long long total_size = 0;
+
+    writer.resetCounter();
+
     for( auto& fl : header.folders ){
         string cdir = fl.folderPath;
         //getFullDir(&fl,&header);
@@ -17,6 +21,7 @@ void fillArchieve( Header& header, BitWriter& writer, const string& ftarget ){
         for(int limit = fileidx + fl.numOfFile; fileidx < limit; ++ fileidx ){
             string fullPath = cdir + "\\" + header.files[fileidx].fileName;
             int size = (header.files[fileidx].size);
+            total_size += size;
 //            cout << fullPath << " " << (header.files[fileidx].size) << endl;
             /*
             ifstream infile( fullPath );
@@ -31,14 +36,15 @@ void fillArchieve( Header& header, BitWriter& writer, const string& ftarget ){
             }
             infile.close();
             */
-            writer.resetCounter();
 
             HuffmanTree T;
             T.compress(writer,fullPath);
 
-            double ratio = (double) (writer.getBitCount() / 8) / (double) size;
         }
     }
+
+    double ratio = (double) (writer.getBitCount() / 8) / (double) total_size * 100.0;
+    cout << ratio << endl;
 }
 
 void extractArchieve( Header& header, BitReader& reader, const string& ftarget ){
